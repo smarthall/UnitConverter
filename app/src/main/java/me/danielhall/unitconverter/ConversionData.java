b8250185e826d885c50ce01fb8d8cf32416d967c
Daniel Hall <daniel@danielhall.me>
2015-06-06 13:10:22 +1000

    Initial commit

---
+++ app/src/main/java/me/danielhall/unitconverter/ConversionData.java
@@ -0,0 +1,8 @@
+package me.danielhall.unitconverter;
+
+/**
+ * Created by daniel on 6/06/15.
+ */
+public class ConversionData {
+    private 
+}
---
+++ app/src/main/java/me/danielhall/unitconverter/ConverterFragment.java
@@ -0,0 +1,65 @@
+package me.danielhall.unitconverter;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ConverterFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ConverterFragment extends Fragment {
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    private int section_number;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param section_number The section to display.
+     * @return A new instance of fragment ConverterFragment.
+     */
+    public static ConverterFragment newInstance(int section_number) {
+        ConverterFragment fragment = new ConverterFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, section_number);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public ConverterFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            section_number = getArguments().getInt(ARG_SECTION_NUMBER);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_converter, container, false);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
